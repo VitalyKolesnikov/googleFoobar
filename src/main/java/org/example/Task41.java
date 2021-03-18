b77722a3a0abb542bd36885b58f108e4c4facb6d
VitalyKolesnikov <vitaly.kolesnikov88@gmail.com>
2021-03-18 17:14:30 +0300

    Level 4 task 1 done

--- src/main/java/org/example/Task41.java
+++ src/main/java/org/example/Task41.java
@@ -1,36 +1,60 @@
 package org.example;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task41 {
 
-    public static void main(String[] args) {
-        System.out.println(choose(5, 2));
-    }
-
-    // [5, 3]
-
     public static int[][] solution(int num_buns, int num_required) {
+        int keyCopies = num_buns - num_required + 1;
 
-        int distinctKeysNum = choose(num_buns, num_required - 1); // 10
-        int copiesPerKeyNum = num_buns - num_required + 1; // 3
+        ArrayList<ArrayList<Integer>> keyRings = new ArrayList<>(num_buns);
+        for (int pos = 0; pos < num_buns; pos++) {
+            keyRings.add(new ArrayList<>());
+        }
 
+        int key = 0;
+        List<int[]> combinations = generate(num_buns, keyCopies);
+        for (int[] keyRing : combinations) {
+            for (int keyIndex : keyRing) {
+                keyRings.get(keyIndex).add(key);
+            }
+            key++;
+        }
 
+        int[][] result = new int[num_buns][];
+        for (int keyRingIndex = 0; keyRingIndex < keyRings.size(); keyRingIndex++) {
+            ArrayList<Integer> keyRing = keyRings.get(keyRingIndex);
+            result[keyRingIndex] = new int[keyRing.size()];
+            for (int i = 0; i < keyRing.size(); i++) {
+                result[keyRingIndex][i] = keyRing.get(i);
+            }
+        }
 
-        return null;
+        return result;
     }
 
-    public static int choose(int n, int k) {
-        int numerator = 1;
-        int denominator = 1;
+    public static List<int[]> generate(int n, int r) {
+        List<int[]> combinations = new ArrayList<>();
+        int[] combination = new int[r];
 
-        for (int i = n; i >= (n - k + 1); i--) {
-            numerator *= i;
+        for (int i = 0; i < r; i++) {
+            combination[i] = i;
         }
 
-        for (int i = k; i >= 1; i--) {
-            denominator *= i;
+        while (combination[r - 1] < n) {
+            combinations.add(combination.clone());
+            int t = r - 1;
+            while (t != 0 && combination[t] == n - r + t) {
+                t--;
+            }
+            combination[t]++;
+            for (int i = t + 1; i < r; i++) {
+                combination[i] = combination[i - 1] + 1;
+            }
         }
-
-        return (numerator / denominator);
+        return combinations;
     }
 
 }
--- src/test/java/org/example/Task41Test.java
+++ src/test/java/org/example/Task41Test.java
@@ -2,54 +2,30 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.example.Task41.choose;
 import static org.example.Task41.solution;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Task41Test {
 
     @Test
-    void choose_5_3_eq_10() {
-        int actual = choose(5, 3);
-        int expected = 10;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void choose_4_4_eq_4() {
-        int actual = choose(4, 4);
-        int expected = 1;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void choose_3_1_eq_3() {
-        int actual = choose(3, 1);
-        int expected = 3;
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
     void case1() {
         int[][] actual = solution(2, 1);
-        int[][] expected = {{0}, {0}};
+        int[][] expected = {
+                {0},
+                {0}
+        };
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void case2() {
-        int[][] actual = solution(5, 3);
+        int[][] actual = solution(4, 4);
         int[][] expected = {
-                {0, 1, 2, 3, 4, 5},
-                {0, 1, 2, 6, 7, 8},
-                {0, 3, 4, 6, 7, 9},
-                {1, 3, 5, 6, 8, 9},
-                {2, 4, 5, 7, 8, 9}
+                {0},
+                {1},
+                {2},
+                {3}
         };
 
         assertArrayEquals(expected, actual);
@@ -57,38 +33,42 @@ class Task41Test {
 
     @Test
     void case3() {
-        int[][] actual = solution(4, 4);
-        int[][] expected = {{0}, {1}, {2}, {3}};
+        int[][] actual = solution(3, 1);
+        int[][] expected = {
+                {0},
+                {0},
+                {0}
+        };
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void case4() {
-        int[][] actual = solution(3, 1);
-        int[][] expected = {{0}, {0}, {0}};
+        int[][] actual = solution(3, 2);
+        int[][] expected = {
+                {0, 1},
+                {0, 2},
+                {1, 2}
+        };
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void case5() {
-        int[][] actual = solution(3, 2);
-        int[][] expected = {{0, 1}, {0, 2}, {1, 2}};
+        int[][] actual = solution(3, 3);
+        int[][] expected = {
+                {0},
+                {1},
+                {2}
+        };
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void case6() {
-        int[][] actual = solution(3, 3);
-        int[][] expected = {{0}, {1}, {2}};
-
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void case7() {
         int[][] actual = solution(1, 1);
         int[][] expected = {{0}};
 
@@ -96,9 +76,51 @@ class Task41Test {
     }
 
     @Test
-    void case8() {
+    void case7() {
         int[][] actual = solution(2, 2);
-        int[][] expected = {{0}, {1}};
+        int[][] expected = {
+                {0},
+                {1}
+        };
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void case8() {
+        int[][] actual = solution(4, 3);
+        int[][] expected = {
+                {0, 1, 2         },
+                {0,       3, 4   },
+                {   1,    3,    5},
+                {      2,    4, 5}
+        };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void case9() {
+        int[][] actual = solution(5, 4);
+        int[][] expected = {
+                {0, 1, 2, 3                  },
+                {0,          4, 5, 6         },
+                {   1,       4,       7, 8   },
+                {      2,       5,    7,    9},
+                {         3,       6,    8, 9}
+        };
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void case10() {
+        int[][] actual = solution(5, 3);
+        int[][] expected = {
+                {0, 1, 2, 3, 4, 5            },
+                {0, 1, 2,          6, 7, 8   },
+                {0,       3, 4,    6, 7,    9},
+                {   1,    3,    5, 6,    8, 9},
+                {      2,    4, 5,    7, 8, 9}
+        };
 
         assertArrayEquals(expected, actual);
     }
